a0b613841fb09af316956053ebd5384435a173b0
shobhitc1 <shobhitc13@gmail.com>
2026-08-26 20:05:58 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,22 @@ import Dexie from 'dexie'
 
 export const db = new Dexie('WeddingFitTrackerDB')
 
+// Version 1: original schema
+db.version(1).stores({
+  exercises: '++id, day',
+  logs: '++id, date, exerciseName',
+  measurements: '++id, date'
+})
+
+// Version 2: drop the old logs table (can't change primary key in place)
 db.version(2).stores({
+  exercises: '++id, day',
+  logs: null,
+  measurements: '++id, date'
+})
+
+// Version 3: recreate logs table with new primary key
+db.version(3).stores({
   exercises: '++id, day',
   logs: 'id, date, exerciseName',
   measurements: '++id, date'
